2d4255fefbe50e255a5deeb9060be2a2fd545419
dkcladlek098 <dkcladlek098@naver.com>
2025-12-05 13:23:15 +0900

    컨트롤러 초기 세팅

--- src/main/java/com/aivle06/bookservice/controller/HelloController.java
+++ src/main/java/com/aivle06/bookservice/controller/HelloController.java
@@ -1,13 +1,53 @@
 package com.aivle06.bookservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.aivle06.bookservice.domain.Book;
+import com.aivle06.bookservice.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 @RestController
+@RequestMapping
+@RequiredArgsConstructor
 public class HelloController {
 
-    @GetMapping("/api/hello")
-    private String hello() {
-        return "Hello World";
+    private final BookService bookService;
+
+    // 새 책 등록 (POST /api/books)
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        Book createdBook = bookService.createBook(book);
+        return new ResponseEntity<>(createdBook, HttpStatus.CREATED);
+    }
+
+    // 전체 목록 조회 (GET /api/books/?page={page_num})
+    @GetMapping
+    public ResponseEntity<List<Book>> getAllBooks() {
+        List<Book> books = bookService.getAllBooks();
+        return ResponseEntity.ok(books);
+    }
+
+    // 특정 ID로 조회 (GET /api/books/{book_id})
+    @GetMapping("/{id}")
+    public ResponseEntity<Book> getBookById(@PathVariable Long id) {
+        Book book = bookService.getBookById(id);
+        return ResponseEntity.ok(book);
+    }
+
+    // 업뎃 (PUT /api/books/{id})
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> updateBook(@PathVariable Long id,
+                                           @RequestBody Book book) {
+        Book updatedBook = bookService.updateBook(id, book);
+        return ResponseEntity.ok(updatedBook);
+    }
+
+    // 삭제 (DELETE /api/books/{id})
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+        return ResponseEntity.noContent().build();
     }
 }
